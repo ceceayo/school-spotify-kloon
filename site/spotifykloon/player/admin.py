372f9d2af6b0ba4c16399bf4bfb16e08478ce60f
Jesse Weikamp <100067092+ceceayo@users.noreply.github.com>
2023-09-11 13:51:59 +0000

    added Music and MusicTrack models to database. Added migrations.

--- site/spotifykloon/player/admin.py
+++ site/spotifykloon/player/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Music, MusicTrack
 
 # Register your models here.
+admin.site.register(Music)
+admin.site.register(MusicTrack)
--- site/spotifykloon/player/models.py
+++ site/spotifykloon/player/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Music(models.Model):
+    title = models.TextField()
+
+
+class MusicTrack(models.Model):
+    title = models.TextField()
+    file = models.FileField(upload_to='static/music/')
+    music = models.ForeignKey(Music, on_delete=models.CASCADE)
+
