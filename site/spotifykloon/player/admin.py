2b382c7456e32814bb25962504bd9f7660de9b44
ceayo <ceayo@proton.me>
2023-09-11 19:10:42 +0200

    Added artist to admin

--- site/spotifykloon/player/admin.py
+++ site/spotifykloon/player/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Music, MusicTrack
+from .models import Music, MusicTrack, Artist
 
 # Register your models here.
 admin.site.register(Music)
 admin.site.register(MusicTrack)
+admin.site.register(Artist)
