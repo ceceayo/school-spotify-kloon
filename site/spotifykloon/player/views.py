02d3182663da78235262bd3bd7d5441d0050936f
ceayo <ceayo@proton.me>
2023-09-11 18:08:56 +0200

    WOOOO! prototype works! - database contains a song - this song is visible on the site - under this song, the versions are listed - clicking on a version will navigate the user towards that file.

--- site/spotifykloon/player/views.py
+++ site/spotifykloon/player/views.py
@@ -1,4 +1,23 @@
 from django.views.generic import TemplateView
+from .models import Music, MusicTrack
+from dataclasses import dataclass
+
+
+@dataclass
+class MusicItem:
+    musicItem: Music
+    tracks: list[MusicTrack]
+
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        music = Music.objects.all()
+        result = []
+        for item in music:
+            tracks = MusicTrack.objects.filter(music=item)
+            result.append(MusicItem(item, tracks))
+        context["music"] = result
+        return context
