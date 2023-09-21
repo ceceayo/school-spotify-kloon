008c0948052f4fc5cffa8b48cb5d9b575a251e13
Jesse Weikamp <100067092+ceceayo@users.noreply.github.com>
2023-09-21 11:38:35 +0000

    now using a django form for authentication

--- site/spotifykloon/accounts/views.py
+++ site/spotifykloon/accounts/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 # Create your views here.
+from .forms import LoginForm
+from django.http import HttpResponseRedirect
 
+    
 def login_page_view(request):
-    if request.method == "GET":
-        return render(request, template_name="login.html")
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = LoginForm(request.POST)
+        print(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/")
+
+    # if a GET (or any other method) we'll create a blank form
     else:
-        raise NotImplementedError("CANT DO THAT YET SORRY OwO")
+        form = LoginForm()
+
+    return render(request, "login.html", {"form": form})
