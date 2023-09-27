4be14a52c7f16800eba979eecb7aff1a5846115b
ceayo <ceayo@proton.me>
2023-09-27 10:11:49 +0200

    Add album field to Music model
    A new field 'album' was added to the Music model to store the relationship between a music track and its album. This will enrich the information available in each music object and enhance the user's search experience. This required creating a new migration. Also, unnecessary whitespaces were removed for clean coding.

--- site/spotifykloon/player/models.py
+++ site/spotifykloon/player/models.py
@@ -1,36 +1,35 @@
 from django.db import models
 
 
-
 class Artist(models.Model):
     name = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
 
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=100, primary_key=True)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} by {self.artist.name}"
+
 
 class Music(models.Model):
     title = models.CharField(max_length=100)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, blank=True, null=True)
-    
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, blank=True, null=True)
+
     def __str__(self):
         return f"{self.artist.name} - {self.title}"
 
 
 class MusicTrack(models.Model):
     title = models.CharField(max_length=100)
-    file = models.FileField(upload_to='static/music/')
+    file = models.FileField(upload_to="static/music/")
     music = models.ForeignKey(Music, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"{self.title} for {self.music.title} by {self.music.artist.name}"
-
-
-class Album(models.Model):
-    title = models.CharField(max_length=100, primary_key=True)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f"{self.title} by {self.artist.name}"
