cd1bbe001b0a6cac33c877446f53fb349527c5c1
ceayo <ceayo@proton.me>
2023-09-27 16:14:54 +0200

    Registered Artist model in admin site
    The Artist model was already registered in the admin site, however, it was placed after the other model registrations. It has been moved to precede other models for consistency and better readability.

--- site/spotifykloon/player/admin.py
+++ site/spotifykloon/player/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Artist, Music, MusicTrack
 
 # Register your models here.
+admin.site.register(Artist)
 admin.site.register(Music)
 admin.site.register(MusicTrack)
-admin.site.register(Artist)
