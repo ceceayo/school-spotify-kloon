aaffeb453d2cf3deb8888b39c4592c96a2176d9f
ceayo <ceayo@proton.me>
2023-09-27 16:57:40 +0200

    Add model tests for Artists, Albums, and Music
    Added a ModelsTest class to provide thorough model testing for the Artist, Album, and Music models. The tests include creation of instances and checking their inclusion in the database, as well as testing relationships between models (e.g., album belongs to artist, music belongs to album). This ensures the correctness and integrity of the model data.

--- site/spotifykloon/player/tests.py
+++ site/spotifykloon/player/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
+from .models import Artist, Album, Music
+
+
 # Create your tests here.
+
+
+class ModelsTest(TestCase):
+    def test_models(self):
+        artist = Artist.objects.create(name="test")
+        self.assertEqual(artist.name, "test")
+        artist.save()
+        self.assertEqual(artist in Artist.objects.all(), True)
+        album = Album.objects.create(title="aaaa", artist=artist)
+        self.assertEqual(album.title, "aaaa")
+        self.assertEqual(album.artist, artist)
+        album.save()
+        self.assertEqual(album in Album.objects.all(), True)
+        music = Music.objects.create(title="bbbb", album=album)
+        self.assertEqual(music.title, "bbbb")
+        self.assertEqual(music.album, album)
+        self.assertEqual(music.album.artist, artist)
+        music.save()
+        self.assertEqual(music in Music.objects.all(), True)
+        self.assertEqual(music.album.artist.name, "test")
