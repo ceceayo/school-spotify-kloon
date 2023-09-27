9b1502849cb31b46ff89efe4c6abf3074c40e28f
ceayo <ceayo@proton.me>
2023-09-27 10:17:59 +0200

    Register Album model in admin
    An Album model was imported and registered in the Django admin. The change was made to make the Album data manageable and searchable in the admin panel.

--- site/spotifykloon/player/admin.py
+++ site/spotifykloon/player/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Music, MusicTrack, Artist
+
+from .models import Music, MusicTrack, Artist, Album
 
 # Register your models here.
+admin.site.register(Album)
 admin.site.register(Music)
 admin.site.register(MusicTrack)
 admin.site.register(Artist)
